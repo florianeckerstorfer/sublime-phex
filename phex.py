7e7db0e575a281533edd378e1c14d74fbf36e940
Florian Eckerstorfer <florian@theroadtojoy.at>
2014-05-06 14:41:15 +0200

    Whitespace stuff

--- phex.py
+++ phex.py
@@ -11,9 +11,9 @@ class Pref:
     def load():
         settings = sublime.load_settings('phex.sublime-settings')
 
-        Pref.default_author    = settings.get('phex_default_author')
-        Pref.default_copyright = settings.get('phex_default_copyright')
-        Pref.default_license   = settings.get('phex_default_license')
+        Pref.default_author     = settings.get('phex_default_author')
+        Pref.default_copyright  = settings.get('phex_default_copyright')
+        Pref.default_license    = settings.get('phex_default_license')
         Pref.default_source_dir = settings.get('phex_default_source_dir')
 
 def plugin_loaded():
